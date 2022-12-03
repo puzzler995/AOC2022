5b0ee352efbc2177f81fa338d4c46c28e31f6baa
puzzler995 <ksmarsee@gmail.com>
2022-12-03 17:36:59 -0500

    finished day 1

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+
+elfFile = open("/home/kat/AOC/day1/input", "r")
+elves = []
+total = 0
+
+for line in elfFile.readlines():
+  if line.strip() == "":
+    elves.append(total)
+    total=0
+  else:
+    total=total+int(line)
+
+elves.sort(reverse=True)
+max = elves[0] + elves[1] + elves[2]
+print(max)
