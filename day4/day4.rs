cbf966b549efd6850db023662b983aba7bd39bb2
puzzler995 <ksmarsee@gmail.com>
2022-12-04 17:40:28 -0500

    cleaned up day4's line parser

--- day4/day4.rs
+++ day4/day4.rs
@@ -4,13 +4,11 @@ fn main(){
   let mut total_overlaps = 0;
 
   for line in input {
-    let mut pairings_list = line.split(',');
-    let mut pair_one = pairings_list.next().unwrap().split('-');
-    let mut pair_two = pairings_list.next().unwrap().split('-');
-    let pair_one_first:i32 = pair_one.next().unwrap().parse().unwrap();
-    let pair_one_second:i32 = pair_one.next().unwrap().parse().unwrap();
-    let pair_two_first:i32 = pair_two.next().unwrap().parse().unwrap();
-    let pair_two_second:i32 = pair_two.next().unwrap().parse().unwrap();
+    let mut pairings_list = line.split(|x| x == ',' || x == '-');
+    let pair_one_first:i32 = pairings_list.next().unwrap().parse().unwrap();
+    let pair_one_second:i32 = pairings_list.next().unwrap().parse().unwrap();
+    let pair_two_first:i32 = pairings_list.next().unwrap().parse().unwrap();
+    let pair_two_second:i32 = pairings_list.next().unwrap().parse().unwrap();
     
     if (pair_one_first<=pair_two_first && pair_one_second >= pair_two_second) || (pair_one_first >=pair_two_first && pair_one_second<=pair_two_second){
       total_full_overlaps += 1;
