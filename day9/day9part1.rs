26f31c25785b3a2adf325dbe8a91b93b6ba2e9db
puzzler995 <ksmarsee@gmail.com>
2022-12-11 00:37:20 -0500

    finished day9 with way too much help from reddit

--- day9/day9part1.rs
+++ day9/day9part1.rs
@@ -1,89 +1,56 @@
-use std::{borrow::Borrow, fs::File, io::Write};
+use std::{collections::HashSet};
+use std::io;
+
+type Coords = (i64, i64);
 
 fn main() {
-  let input = include_str!("input");
-  let _maxsize = (500,500);
-  let mut head_current_x = 250;
-  let mut head_current_y = 250;
-  let mut tail_current_x = 250;
-  let mut tail_current_y = 250;
-  let mut spots = 1;
-  let mut spot_list:Vec<(usize, usize)> = Vec::new();
-  spot_list.push((tail_current_x as usize, tail_current_y as usize));
-  for line in input.lines() {
-    let mut instructions = line.split(" ");
-    let direction = instructions.next().unwrap();
-    let distance:usize = instructions.next().unwrap().parse().unwrap();
-    for _step in 0..distance {
-      match direction {
-          "U" => {
-            head_current_y+=1;
-          }
-          "D" => {
-            head_current_y-=1;
-          }
-          "R" => {
-            head_current_x+=1;
-          }
-          "L" => {
-            head_current_x-=1;
-          }
-          _ => {}
-      }
-      //print_state(maxsize.1, maxsize.0, thisisdumb.borrow(), head_current_x, head_current_y, tail_current_x, tail_current_y);
-      if ((head_current_x-tail_current_x)==2) && (head_current_y==tail_current_y) {
-        tail_current_x+=1;
-      } else if ((tail_current_x-head_current_x)==2) && (head_current_y == tail_current_y){
-        tail_current_x-=1;
-      } else if ((head_current_y-tail_current_y)==2) && (head_current_x == tail_current_x){
-        tail_current_y+=1;
-      } else if ((tail_current_y-head_current_y)==2) && (head_current_x == head_current_x) {
-        tail_current_y-=1;
-      } else if (((head_current_x-tail_current_x)==1) && ((head_current_y-tail_current_y)==2))
-            || (((head_current_x-tail_current_x)==2) && ((head_current_y-tail_current_y)==1)){
-        tail_current_x+=1;
-        tail_current_y+=1;
-      } else if (((head_current_x-tail_current_x)==1) && ((tail_current_y-head_current_y)==2))
-            || (((head_current_x-tail_current_x)==2) && ((tail_current_y-head_current_y)==1)){
-        tail_current_x+=1;
-        tail_current_y-=1;
-      } else if (((tail_current_x-head_current_x)==1) && ((tail_current_y-head_current_y)==2))
-            || (((tail_current_x-head_current_x)==2) && ((tail_current_y-head_current_y)==1)){
-        tail_current_x-=1;
-        tail_current_y-=1;
-      } else if (((tail_current_x-head_current_x)==1) && ((head_current_y-tail_current_y)==2))
-            || (((tail_current_x-head_current_x)==2) && ((head_current_y-tail_current_y)==1)){
-        tail_current_x-=1;
-        tail_current_y+=1;
-      }
-      let spot_state = spot_list.contains(&(tail_current_x as usize, tail_current_y as usize));
-      if !spot_state {
-        spots += 1;
-        spot_list.push((tail_current_x as usize, tail_current_y as usize));
-      }
-      //print_state(maxsize.1, maxsize.0, thisisdumb.borrow(), head_current_x, head_current_y, tail_current_x, tail_current_y);
-    }
-  }
-  println!("Spots {}", spots);
+  let input: Vec<String> = io::stdin().lines().flatten().collect();
+
+  let mut rope = vec![(0,0); 2];
+  println!("{}", moving(&input, &mut rope));
+
+  let mut rope = vec![(0,0); 10];
+  println!("{}", moving(&input, &mut rope));
+  
 }
 
-fn print_state(maxy:usize, maxx:usize, useds:&Vec<(usize, usize)>, headx:i32, heady:i32, tailx: i32, taily: i32) {
-  let mut output = File::create("/home/kat/AOC2022/day9/output").expect("create failed");
-  for y in 0..maxy {
-    let mut opline = String::new();
-    for x in 0..maxx{
-      if (x==(headx as usize)) && (y==heady as usize) {
-        opline.push('H');
-      } else if (x==(tailx as usize)) && (y==taily as usize) {
-        opline.push('T');
-      } else if useds.contains(&(x,y)) {
-        opline.push('#');
-      }else {
-        opline.push('.');
+fn moving(moves: &[String], rope: &mut [Coords]) -> usize {
+  let mut spot_list: HashSet<Coords> = HashSet::new();
+  for mov in moves {
+    let (x, y, d) = match mov.split_at(2) {
+        ("R ", d) => (1, 0, d),
+        ("L ", d) => (-1, 0, d),
+        ("U ", d) => (0, 1, d),
+        ("D ", d) => (0, -1, d),
+        (_, _) => unreachable!(),  
+    };
+    for _ in 0..d.parse::<usize>().unwrap() {
+      rope[0].0 += x;
+      rope[0].1 += y;
+      for knot in 1..rope.len() {
+        if let Some(coor) = adjust(&rope[knot], &rope[knot-1]) {
+          rope[knot] = coor;
+        } else {
+          break;
+        }
+        
       }
-    }
-    opline.push_str("\n");
-    output.write(opline.as_bytes()).expect("write failed");
+      spot_list.insert(*rope.last().unwrap());
+    }   
   }
-  output.write("\n".as_bytes()).expect("write failed");
+  spot_list.len()
+}
+fn adjust(tail: &Coords, head: &Coords) -> Option<Coords> {
+  let delta_x = tail.0 - head.0;
+  let delta_y = tail.1 - head.1;
+
+  if (delta_x == 2 || delta_x == -2) && (delta_y == 2 || delta_y == -2) {
+    Some((head.0 + delta_x.clamp(-1, 1), head.1 + delta_y.clamp(-1, 1)))
+  } else if delta_x == 2 || delta_x == -2 {
+    Some((head.0 + delta_x.clamp(-1, 1), head.1))
+  } else if delta_y == 2 || delta_y == -2 {
+    Some((head.0, head.1 + delta_y.clamp(-1, 1)))
+  } else {
+      None
+  }
 }
